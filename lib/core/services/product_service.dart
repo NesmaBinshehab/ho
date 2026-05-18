b92844805c97a8940c7fd78ae31d3ce99abeb21b
unknown <nesmabinshehab@gmail.com>
2026-05-18 02:11:04 -0700

    clear main

--- lib/core/services/product_service.dart
+++
@@ -1,9 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-class ProductService {
-  final _ref = FirebaseFirestore.instance.collection('products');
-
-  Stream<QuerySnapshot> getProducts() {
-    return _ref.snapshots();
-  }
-}
